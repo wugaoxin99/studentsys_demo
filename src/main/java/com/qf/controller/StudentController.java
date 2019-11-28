d8ae41279dfd7c7db13c53a60dab10c03e88b561
yuchuanmao <1337949783@qq.com>
2019-11-28 21:48:15 +0800

    你好新世界

--- src/main/java/com/qf/controller/StudentController.java
+++ src/main/java/com/qf/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.sql.Date;
 import java.util.List;
